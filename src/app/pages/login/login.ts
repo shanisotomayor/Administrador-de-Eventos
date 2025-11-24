9196830b0f7825fe66a04eeb81d7ad6b4a838ff1
shanisotomayor <1007920231@unajma.edu.pe>
2025-11-24 03:08:13 -0500

    Agregar login y registro funcionales con FirebaseAuth

--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../core/auth/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
-  templateUrl: './login.html',
-  styleUrl: './login.scss',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './login.html'
 })
 export class Login {
+  email = '';
+  password = '';
+  loading = false;
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  async ingresar() {
+    this.loading = true;
+    try {
+      await this.auth.login(this.email, this.password);
+      this.router.navigate(['/dashboard']);
+    } catch (e:any) {
+      alert(e.message || 'Error al iniciar sesión');
+    } finally {
+      this.loading = false;
+    }
+  }
 }
--- src/app/pages/register/register.ts
+++ src/app/pages/register/register.ts
@@ -1,11 +1,31 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../core/auth/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
-  imports: [],
-  templateUrl: './register.html',
-  styleUrl: './register.scss',
+  standalone: true,
+  imports: [FormsModule],
+  templateUrl: './register.html'
 })
 export class Register {
+  email = '';
+  password = '';
+  loading = false;
 
+  constructor(private auth: AuthService, private router: Router) {}
+
+  async crearCuenta() {
+    this.loading = true;
+    try {
+      await this.auth.register(this.email, this.password);
+      alert('Cuenta creada. Inicia sesión.');
+      this.router.navigate(['/login']);
+    } catch (e:any) {
+      alert(e.message || 'Error al crear cuenta');
+    } finally {
+      this.loading = false;
+    }
+  }
 }
