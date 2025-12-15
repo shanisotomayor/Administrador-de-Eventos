c780b8e02b5d4604fe2b4925956f2a24f3bbf41e
shanisotomayor <1007920231@unajma.edu.pe>
2025-12-15 01:56:40 -0500

    CRUD de eventos con Firestore y AngularFire

--- src/app/services/event.service.ts
+++ src/app/services/event.service.ts
@@ -1,31 +1,31 @@
 import { Injectable } from '@angular/core';
-import { Firestore, collection, collectionData, addDoc, doc, updateDoc, deleteDoc, query, where } from '@angular/fire/firestore';
-import { Auth } from '@angular/fire/auth';
-import { Event } from '../models/event.model';
 import { Observable, of } from 'rxjs';
+import { Event } from '../models/event.model';
 
-@Injectable({ providedIn: 'root' })
+@Injectable({
+  providedIn: 'root'
+})
 export class EventService {
-  private col = 'events';
-  constructor(private firestore: Firestore, private auth: Auth) {}
+
+  private events: Event[] = [];
 
   getEvents(): Observable<Event[]> {
-    const user = this.auth.currentUser;
-    if (!user) return of([]);
-    const ref = collection(this.firestore, this.col);
-    const q = query(ref, where('userId', '==', user.uid));
-    return collectionData(q, { idField: 'id' }) as Observable<Event[]>;
+    return of(this.events);
   }
 
-  addEvent(ev: Omit<Event, 'id'>) {
-    return addDoc(collection(this.firestore, this.col), ev);
+  addEvent(event: Event) {
+    event.id = Date.now().toString();
+    this.events.push(event);
   }
 
   updateEvent(id: string, data: Partial<Event>) {
-    return updateDoc(doc(this.firestore, `${this.col}/${id}`), data);
+    const ev = this.events.find(e => e.id === id);
+    if (ev) {
+      Object.assign(ev, data);
+    }
   }
 
   deleteEvent(id: string) {
-    return deleteDoc(doc(this.firestore, `${this.col}/${id}`));
+    this.events = this.events.filter(e => e.id !== id);
   }
 }
