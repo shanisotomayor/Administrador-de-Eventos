51b2403cee25c417ac08446c784923c3de3af8b1
shanisotomayor <1007920231@unajma.edu.pe>
2025-12-15 02:19:39 -0500

    Protección de rutas y cierre de sesión

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,16 +1,30 @@
 import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { Dashboard } from './pages/dashboard/dashboard';
 import { EventsComponent } from './pages/events/events';
 import { CreateEventComponent } from './pages/create-event';
+import { authGuard } from './guards/auth-guard';
 
 export const routes: Routes = [
   { path: 'login', component: Login },
   { path: 'register', component: Register },
 
-  // RUTAS PROTEGIDAS
-  { path: 'events', component: EventsComponent },
-  { path: 'create', component: CreateEventComponent },
+  {
+    path: 'dashboard',
+    component: Dashboard,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'events',
+    component: EventsComponent,
+    canActivate: [authGuard]
+  },
+  {
+    path: 'create',
+    component: CreateEventComponent,
+    canActivate: [authGuard]
+  },
 
-  { path: '', redirectTo: 'login', pathMatch: 'full' }
+  { path: '', redirectTo: 'events', pathMatch: 'full' }
 ];
--- src/app/pages/dashboard/dashboard.ts
+++ src/app/pages/dashboard/dashboard.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../../core/auth/auth.service';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
-  templateUrl: './dashboard.html',
-  styleUrl: './dashboard.scss',
+  standalone: true,
+  templateUrl: './dashboard.html'
 })
 export class Dashboard {
 
+  constructor(
+    private auth: AuthService,
+    private router: Router
+  ) {}
+
+  logout() {
+    this.auth.logout();
+    this.router.navigate(['/login']);
+  }
 }
